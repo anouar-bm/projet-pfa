1d070ae73b00e3be640328cb15cad182560496ef
anouar-bm <anouar.benmoussa@emsi-edu.ma>
2024-04-26 14:52:41 +0100

    essai 2 pour rendre static

--- dreamtravel_project/dreamtravel_project/settings.py
+++ dreamtravel_project/dreamtravel_project/settings.py
@@ -117,14 +117,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_URL = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- dreamtravel_project/dreamtravel_project/urls.py
+++ dreamtravel_project/dreamtravel_project/urls.py
@@ -17,9 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from app_siteweb.views import index
+
+# from django.urls import settings
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 #.views.py import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include ("app_siteweb.urls"))
    # path("",index)
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
