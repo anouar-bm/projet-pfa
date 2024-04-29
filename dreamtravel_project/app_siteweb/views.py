9eb7cd3c6811d75093e5d1d200111d5c456f9d3e
anouar-bm <anouar.benmoussa@emsi-edu.ma>
2024-04-29 19:10:43 +0100

    hotels

--- dreamtravel_project/app_siteweb/views.py
+++ dreamtravel_project/app_siteweb/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from app_siteweb.models import Hotel
 
 def index(request):
     return render(request, 'accueil/index.html')
 
 def login(request):
     return render(request, 'accueil/login-register.html')
+
+def dict_hotels(request):
+    hotels = Hotel.objects.all()
+
+    return render(request, 'accueil/hotels.html', {'hotel': hotels})
 
 def Atlas(request):
     return render(request, 'accueil/atlass.html')
