ba9b3a09dde91353ec017a8851946b4b0bbd1d74
anouar-bm <anouar.benmoussa@emsi-edu.ma>
2024-05-02 13:42:56 +0100

    chatbot
    ajout model chatbot

--- dreamtravel_project/app_siteweb/models.py
+++ dreamtravel_project/app_siteweb/models.py
@@ -66,6 +66,16 @@ class Client(models.Model):
         # Afficher le nom du client et le nom complet de l'utilisateur associé
         return f"{self.nom} - {self.user.first_name} {self.user.last_name}"
     
+class ChatBot(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="GeminiUser", null=True
+    )
+    text_input = models.CharField(max_length=500)
+    gemini_output = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    def __str__(self):
+        return self.text_input
+
 class Like(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
