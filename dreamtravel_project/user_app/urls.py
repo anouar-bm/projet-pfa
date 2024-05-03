f60a16e52056a60403c4d2e223b026155641b09a
anouar-bm <anouar.benmoussa@emsi-edu.ma>
2024-05-03 16:39:39 +0100

    /

--- dreamtravel_project/user_app/urls.py
+++ dreamtravel_project/user_app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', LogoutView.as_view(http_method_names=['post']), name='logout'),  # Accepter POST uniquement
+    
 ]
